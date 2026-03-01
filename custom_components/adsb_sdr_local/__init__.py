b954e338c2909ffa9af0c9a1c8c1ea5f57368c37
earnshawglenn8-sudo <earnshawglenn8@gmail.com>
2026-03-01 17:55:37 +0000

    Add files via upload

---
+++ custom_components/adsb_sdr_local/__init__.py
@@ -0,0 +1,20 @@
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+
+from .const import DOMAIN, PLATFORMS
+from .coordinator import LocalAdsbCoordinator
+
+
+async def async_setup(hass: HomeAssistant, config: dict) -> bool:
+    return True
+
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    coordinator = LocalAdsbCoordinator(hass, entry)
+    await coordinator.async_config_entry_first_refresh()
+
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][entry.entry_id] = coordinator
+
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+    return True
---
+++ custom_components/adsb_sdr_local/const.py
@@ -0,0 +1,13 @@
+DOMAIN = "adsb_sdr_local"
+
+CONF_HOST = "host"
+CONF_PORT = "port"
+CONF_PATH = "path"
+CONF_LAT = "lat"
+CONF_LON = "lon"
+
+DEFAULT_PORT = 8080
+DEFAULT_PATH = "/data/aircraft.json"
+DEFAULT_SCAN_INTERVAL = 10
+
+PLATFORMS = ["sensor"]
